45e65d12c405eeae2ba59d433cf4265f07c90a6a
James Coggan <jamesMark.Coggan@aexp.cxom>
2018-07-17 10:56:11 +0100

    Added button logic

--- worshopapps/things/src/main/java/com/jamescoggan/workshopapp/ThingsActivity.kt
+++ worshopapps/things/src/main/java/com/jamescoggan/workshopapp/ThingsActivity.kt
@@ -3,6 +3,7 @@ package com.jamescoggan.workshopapp
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.google.android.things.pio.Gpio
+import com.google.android.things.pio.GpioCallback
 import com.google.android.things.pio.PeripheralManager
 import com.jamescoggan.workshopapp.port.gpioForButton
 import com.jamescoggan.workshopapp.port.gpioForLED
@@ -15,6 +16,7 @@ import timber.log.Timber
 class ThingsActivity : AppCompatActivity() {
 
     private lateinit var led: Gpio
+    private lateinit var button: Gpio
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,17 +28,20 @@ class ThingsActivity : AppCompatActivity() {
 
         led = PeripheralManager.getInstance().openGpio(gpioForLED) // Open the LED port
         led.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW) // Set the port to OUT with initial LOW
-
-        while(true){
-            val newValue = !led.value // Invert the LED value
-            Timber.d("Setting the led to $newValue")
-            led.value = newValue
-            Thread.sleep(1000) // Wait 1 Second
+        button = PeripheralManager.getInstance().openGpio(gpioForButton) // Open the Button Port
+        button.setDirection(Gpio.DIRECTION_IN) // Set the port to IN for reading
+        button.setActiveType(Gpio.ACTIVE_LOW) // Active type is low, when button is grounded
+        button.setEdgeTriggerType(Gpio.EDGE_BOTH) // We want to detect on press and release
+        button.registerGpioCallback { gpio ->
+            Timber.d("Button pressed: ${!gpio.value}") // Read and print the GPIO value
+            led.value = !gpio.value // Set the LED to the value, the reading is inverted
+            true // Return true so we continue monitoring the button events
         }
     }
 
     override fun onStop() {
         led.close()
+        button.close()
         super.onStop()
     }
 }
