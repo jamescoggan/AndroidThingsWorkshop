04f019bf6abc9c81e78330d7a9bff6e135bd8243
James Coggan <jamesMark.Coggan@aexp.cxom>
2018-07-17 21:07:37 +0100

    Added the temperature sensor reading

--- worshopapps/things/src/main/java/com/jamescoggan/workshopapp/ThingsActivity.kt
+++ worshopapps/things/src/main/java/com/jamescoggan/workshopapp/ThingsActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.google.android.things.pio.Gpio
 import com.google.android.things.pio.GpioCallback
+import com.google.android.things.pio.I2cDevice
 import com.google.android.things.pio.PeripheralManager
 import com.jamescoggan.workshopapp.port.gpioForButton
 import com.jamescoggan.workshopapp.port.gpioForLED
@@ -17,6 +18,7 @@ class ThingsActivity : AppCompatActivity() {
 
     private lateinit var led: Gpio
     private lateinit var button: Gpio
+    private lateinit var tempSensor : I2cDevice
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,15 +35,23 @@ class ThingsActivity : AppCompatActivity() {
         button.setActiveType(Gpio.ACTIVE_LOW) // Active type is low, when button is grounded
         button.setEdgeTriggerType(Gpio.EDGE_BOTH) // We want to detect on press and release
         button.registerGpioCallback { gpio ->
-            Timber.d("Button pressed: ${!gpio.value}") // Read and print the GPIO value
+            Timber.d("Button pressed: ${gpio.value}") // Read and print the GPIO value
             led.value = !gpio.value // Set the LED to the value, the reading is inverted
             true // Return true so we continue monitoring the button events
         }
+
+        tempSensor = PeripheralManager.getInstance().openI2cDevice(i2cForTempSensor, 0x4A)
+        tempSensor.write(ByteArray(1), 1)
+        val buffer = ByteArray(1)
+        tempSensor.read(buffer, 1)
+        val temperature: Int = buffer[0].toInt() and 0xff
+        Timber.d("Current temperature: $temperature")
     }
 
     override fun onStop() {
         led.close()
         button.close()
+        tempSensor.close()
         super.onStop()
     }
 }
